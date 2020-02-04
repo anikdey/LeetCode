93ba3b0f4e17375faefe85462efa24ea1fffb9b3
Anik Dey <anikdey@Aniks-iMac.local>
2020-02-04 22:57:04 +0600

    search insert position solved

---
+++ src/easy/SearchInsertPosition.java
@@ -0,0 +1,21 @@
+package easy;
+
+public class SearchInsertPosition {
+
+    public static void main(String[] args) {
+        int[] nums = {1,3,5,6};
+        int result = searchInsert(nums, 0);
+        System.out.println(result);
+    }
+
+    public static int searchInsert(int[] nums, int target) {
+        int i = 0;
+        for(i=0; i<nums.length; i++) {
+            if(nums[i]>=target) {
+                return i;
+            }
+        }
+        return i;
+    }
+
+}
