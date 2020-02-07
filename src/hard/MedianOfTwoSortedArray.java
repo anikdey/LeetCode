fa31c89df78932aea2e8658004b48c2cacc65e0a
Anik Dey <anikdey@Aniks-iMac.local>
2020-02-07 23:05:20 +0600

    median of two sorted array solved but not optimum

--- src/hard/MedianOfTwoSortedArray.java
+++ src/hard/MedianOfTwoSortedArray.java
@@ -3,18 +3,22 @@ package hard;
 public class MedianOfTwoSortedArray {
 
     public static void main(String[] args) {
-        int[] nums1 = {1, 3};
-        int[] nums2 = {2};
-        mergeArray(nums1, nums2);
+        int[] nums1 = {1, 2};
+        int[] nums2 = {3, 4};
+
+//        int[] nums1 = {1, 3, 5, 6, 7};
+//        int[] nums2 = {2, 4, 8, 9, 10};
+
+        System.out.println(findMedianSortedArrays(nums1, nums2));
+
+        //mergeArray(nums1, nums2);
     }
 
     public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        double median = 0;
-
         if(nums1.length == 0){
             int mid = nums2.length / 2;
             if(nums2.length % 2 == 0) {
-                return (nums2[mid]+nums2[mid-1])/2;
+                return (nums2[mid]+nums2[mid-1])/2.0;
             } else {
                 return nums2[mid];
             }
@@ -23,22 +27,29 @@ public class MedianOfTwoSortedArray {
         if(nums2.length == 0){
             int mid = nums1.length / 2;
             if(nums1.length % 2 == 0) {
-                return (nums1[mid]+nums1[mid-1])/2;
+                return (nums1[mid]+nums1[mid-1])/2.0;
             } else {
                 return nums1[mid];
             }
         }
-        return median;
+
+        int[] mergedArray = mergeArray(nums1, nums2);
+        int mid = mergedArray.length / 2;
+        if(mergedArray.length % 2 == 0) {
+            return ( mergedArray[mid]+mergedArray[mid-1] ) / 2.0;
+        } else {
+            return mergedArray[mid];
+        }
     }
 
-    private static void mergeArray(int[] nums1, int[] nums2) {
+    private static int[] mergeArray(int[] nums1, int[] nums2) {
         int[] newArray = new int[nums1.length + nums2.length];
 
         int i = 0;
         int j = 0;
         int counter = 0;
 
-        while (i<nums1.length-1 && j<nums2.length-1) {
+        while (i<=nums1.length-1 && j<=nums2.length-1) {
             if(nums1[i]<nums2[j]) {
                 newArray[counter] = nums1[i];
                 i++;
@@ -49,13 +60,13 @@ public class MedianOfTwoSortedArray {
             counter++;
         }
 
-        for(int x=j; x<nums2.length-1; x++) {
-            newArray[counter] = nums2[j];
+        for(int x=j; x<nums2.length; x++) {
+            newArray[counter] = nums2[x];
             counter++;
         }
 
-        for(int x=i; x<nums1.length-1; x++) {
-            newArray[counter] = nums1[j];
+        for(int x=i; x<nums1.length; x++) {
+            newArray[counter] = nums1[x];
             counter++;
         }
 
@@ -63,8 +74,7 @@ public class MedianOfTwoSortedArray {
             System.out.print(num+", ");
         }
         System.out.println();
-
-
+        return newArray;
     }
 
 }
