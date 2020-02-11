7ec0761d9ddbb53f9c83d92946abe5388f00242c
Anik Dey <anik@bs-23.net>
2020-02-11 14:34:16 +0600

    length of longest sub string solved

--- src/medium/LongestSubstringWithoutRepeatingCharacters.java
+++ src/medium/LongestSubstringWithoutRepeatingCharacters.java
@@ -6,8 +6,9 @@ import java.util.Map;
 public class LongestSubstringWithoutRepeatingCharacters {
 
     public static void main(String[] args) {
-        //System.out.println(lengthOfLongestSubstring("abcabcbb"));
-        //System.out.println(lengthOfLongestSubstring("sdfabdcsfab"));
+        System.out.println(lengthOfLongestSubstring("fgabcabcbb"));
+        System.out.println(lengthOfLongestSubstring("sdfabdcsfab"));
+        System.out.println(lengthOfLongestSubstring("sdfabdcsfag"));
         System.out.println(lengthOfLongestSubstring("dvdf"));
     }
 
@@ -23,14 +24,18 @@ public class LongestSubstringWithoutRepeatingCharacters {
         for(int i=0; i<length; i++) {
             Character currentChar = s.charAt(i);
             if(map.containsKey(currentChar)){
-                start = map.get(currentChar) + 1;
+                int index = map.get(currentChar) + 1;
+                //start = map.get(currentChar) + 1;
+                if(start<index){
+                    start = index;
+                }
             }
+            int windowLength = i - start + 1;
+            max = Math.max(max, windowLength);
             map.put(currentChar, i);
-            int windowLength = i - start;
-            //if(map.size()>)
-
         }
         return max;
     }
+
 
 }
